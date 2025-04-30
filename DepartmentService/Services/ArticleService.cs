99d370b6b3a77bfffdd224794c1688943d8e27bd
VuVanThanh <vut4262@gmail.com>
2025-04-30 16:26:20 +0700

    te

--- DepartmentService/Services/ArticleService.cs
+++ DepartmentService/Services/ArticleService.cs
@@ -176,7 +176,6 @@ namespace DepartmentService.Services
             
                 try
                 {
-                    
                     string articleType = $"{year} {majorCode}";
 
                     
